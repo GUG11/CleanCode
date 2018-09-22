9181bf8f0926c68191fc0439bbc0ccd058dd2be1
GUG11 <hyzhang2112@gmail.com>
2018-09-21 22:58:11 -0700

    Clean Code Practice Initial Version. This is a monolithic and messy file.

---
+++ README.md
@@ -0,0 +1,3 @@
+Robert C. Martin Clean Code - A Handbook of Agile Software Craftsmanship.
+
+This repo is a practice of step-by-step refinement of an argument parser. 
---
+++ src/test/java/com/objectmentor/utilities/args/ArgsTest.java
@@ -0,0 +1,38 @@
+package com.objectmentor.utilities.args;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ArgsTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ArgsTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ArgsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
