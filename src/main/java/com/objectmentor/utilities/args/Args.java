e7d3798630a153229565dd715be1f979c6a25466
GUG11 <hyzhang2112@gmail.com>
2018-09-23 19:10:50 -0700

    1. Move ArgsException to a separate file. 2. Add tests for ArgsException

--- src/main/java/com/objectmentor/utilities/args/Args.java
+++ src/main/java/com/objectmentor/utilities/args/Args.java
@@ -2,37 +2,23 @@
  * args: -l -p 8080 -d thrift */
 package com.objectmentor.utilities.args;
 
-import java.text.ParseException;
+import com.objectmentor.utilities.args.ArgsException;
 import java.util.*;
 
 public class Args {
   private String schema;
-  private boolean valid = true;
   private Set<Character> unexpectedArguments = new TreeSet<Character>();
   private Map<Character, ArgumentMarshaler> marshalers = new HashMap<Character, ArgumentMarshaler>();
   private Set<Character> argsFound = new HashSet<Character>();
   private Iterator<String> currentArgument;
-  private char errorArgumentId = '\0';
-  private String errorParameter = "TILT";
-  private ErrorCode errorCode = ErrorCode.OK;
   private List<String> argsList;
-
-  private enum ErrorCode {
-    OK,
-    MISSING_STRING,
-    MISSING_INTEGER,
-    INVALID_INTEGER,
-    MISSING_DOUBLE,
-    INVALID_DOUBLE,
-    UNEXPECTED_ARGUMENT
-  };
 
   public static void main(String[] args) {
     try {
       Args arg = new Args("l", args);
       boolean logging = arg.getBoolean('l');
       executeApplication(logging);
-    } catch (ParseException e) {
+    } catch (ArgsException e) {
       System.out.println(e.toString());
     }
   }
@@ -41,25 +27,18 @@ public class Args {
     System.out.printf("logging: %b\n", logging);
   }
 
-  public Args(String schema, String[] args) throws ParseException {
+  public Args(String schema, String[] args) throws ArgsException {
     this.schema = schema;
     argsList = Arrays.asList(args);
-    valid = parse();
+    parse();
   }
 
-  private boolean parse() throws ParseException {
-    if (schema.length() == 0 && argsList.size() == 0) {
-      return true;
-    }
+  private void parse() throws ArgsException {
     parseSchema();
-    try {
-      parseArguments();
-    } catch (ArgsException e) {
-    }
-    return valid;
+    parseArguments();
   }
 
-  private boolean parseSchema() throws ParseException {
+  private boolean parseSchema() throws ArgsException {
     for (String element : schema.split(",")) {
       if (element.length() > 0) {
         String trimmedElement = element.trim();
@@ -69,7 +48,7 @@ public class Args {
     return true;
   }
 
-  private void parseSchemaElement(String element) throws ParseException {
+  private void parseSchemaElement(String element) throws ArgsException {
     char elementId = element.charAt(0);
     String elementTail = element.substring(1);
     validateSchemaElementId(elementId);
@@ -82,17 +61,13 @@ public class Args {
     } else if (elementTail.equals("##")) {
       marshalers.put(elementId, new DoubleArgumentMarshaler());
     } else {
-      throw new ParseException(
-          String.format(
-            "Argument: %c has invalid format: %s.", elementId, elementTail), 0);
+      throw new ArgsException(ArgsException.ErrorCode.INVALID_FORMAT, elementId, elementTail);
     }
   }
 
-  private void validateSchemaElementId(char elementId) throws ParseException {
+  private void validateSchemaElementId(char elementId) throws ArgsException {
     if (!Character.isLetter(elementId)) {
-      throw new ParseException(
-          "Bad character:" + elementId + " in Args format: " + schema, 0
-      );
+      throw new ArgsException(ArgsException.ErrorCode.INVALID_ARGUMENT_NAME, elementId, null);
     }
   }
 
@@ -120,9 +95,7 @@ public class Args {
     if (setArgument(argChar)) {
       argsFound.add(argChar);
     } else {
-      unexpectedArguments.add(argChar);
-      errorCode = ErrorCode.UNEXPECTED_ARGUMENT;
-      valid = false;
+      throw new ArgsException(ArgsException.ErrorCode.UNEXPECTED_ARGUMENT, argChar, null);
     }
   }
 
@@ -135,8 +108,7 @@ public class Args {
       m.set(currentArgument);
       return true;
     } catch (ArgsException e) {
-      valid = false;
-      errorArgumentId = argChar;
+      e.setErrorArgumentId(argChar);
       throw e;
     }
   }
@@ -151,37 +123,6 @@ public class Args {
     } else {
       return "";
     }
-  }
-
-  public String errorMessage() throws Exception {
-    switch (errorCode) {
-      case OK:
-        throw new Exception("TILT: Should not get here.");
-      case UNEXPECTED_ARGUMENT:
-        return unexpectedArgumentMessage();
-      case MISSING_STRING:
-        return String.format("Could not find string parameter for -%c.", errorArgumentId);
-      case INVALID_INTEGER:
-        return String.format("Argument -%c expects an integer but was '%s'.",
-            errorArgumentId, errorParameter);
-      case MISSING_INTEGER:
-        return String.format("Could not find integer parameter for -%c.", errorArgumentId);
-      case INVALID_DOUBLE:
-        return String.format("Argument -%c expects a double but was '%s'.",
-            errorArgumentId, errorParameter);
-      case MISSING_DOUBLE:
-        return String.format("Could not find double parameter for -%c.", errorArgumentId);
-    }
-    return "";
-  }
-
-  private String unexpectedArgumentMessage() {
-    StringBuffer message = new StringBuffer("Argument(s) -");
-    for (char c: unexpectedArguments) {
-      message.append(c);
-    }
-    message.append(" unexpected.");
-    return message.toString();
   }
 
   public boolean getBoolean(char arg) {
@@ -226,10 +167,6 @@ public class Args {
     return argsFound.contains(arg);
   }
 
-  public boolean isValid() {
-    return valid;
-  }
-
   private interface ArgumentMarshaler {
     void set(Iterator<String> currentArgument) throws ArgsException;
     Object get();
@@ -254,8 +191,7 @@ public class Args {
       try {
         stringValue = currentArgument.next();
       } catch (NoSuchElementException e) {
-        errorCode = ErrorCode.MISSING_STRING;
-        throw new ArgsException();
+        throw new ArgsException(ArgsException.ErrorCode.MISSING_STRING);
       }
     }
 
@@ -273,12 +209,9 @@ public class Args {
         parameter = currentArgument.next();
         intValue = Integer.parseInt(parameter);
       } catch (NoSuchElementException e) {
-        errorCode = ErrorCode.MISSING_INTEGER;
-        throw new ArgsException();
+        throw new ArgsException(ArgsException.ErrorCode.MISSING_INTEGER);
       } catch (NumberFormatException e) {
-        errorCode = ErrorCode.INVALID_INTEGER;
-        errorParameter = parameter;
-        throw new ArgsException();
+        throw new ArgsException(ArgsException.ErrorCode.INVALID_INTEGER, parameter);
       }
     }
 
@@ -296,12 +229,9 @@ public class Args {
         parameter = currentArgument.next();
         doubleValue = Double.parseDouble(parameter);
       } catch (NoSuchElementException e) {
-        errorCode = ErrorCode.MISSING_DOUBLE;
-        throw new ArgsException();
+        throw new ArgsException(ArgsException.ErrorCode.MISSING_DOUBLE);
       } catch (NumberFormatException e) {
-        errorParameter = parameter;
-        errorCode = ErrorCode.INVALID_DOUBLE;
-        throw new ArgsException();
+        throw new ArgsException(ArgsException.ErrorCode.INVALID_DOUBLE, parameter);
       }
     }
 
@@ -309,6 +239,4 @@ public class Args {
       return doubleValue;
     }
   }
-
-  private class ArgsException extends Exception {}
 }
--- src/test/java/com/objectmentor/utilities/args/ArgsTest.java
+++ src/test/java/com/objectmentor/utilities/args/ArgsTest.java
@@ -36,94 +36,116 @@ public class ArgsTest
     public void testBooleanArgs() throws Exception
     {
       String schema = "l,t,p";
-      String[] arguments = new String[]{"-lpx"};
+      String[] arguments = new String[]{"-lp"};
       Args arg = new Args(schema, arguments);
       assertTrue(arg.getBoolean('l'));
       assertTrue(arg.getBoolean('p'));
       assertFalse(arg.getBoolean('t'));
-      assertFalse(arg.isValid());
       assertEquals(arg.cardinality(), 2);
-      assertEquals(arg.errorMessage(), "Argument(s) -x unexpected.");
+    }
+
+    public void testUnexpected() throws Exception
+    {
+      String schema = "";
+      String[] arguments = new String[]{"-x"};
+      try {
+        Args arg = new Args(schema, arguments);
+        fail();
+      } catch (ArgsException e) {
+        assertEquals(e.getErrorCode(), ArgsException.ErrorCode.UNEXPECTED_ARGUMENT);
+        assertEquals(e.getErrorArgumentId(), 'x');
+      }
     }
 
     public void testStringArgs() throws Exception
     {
       String schema = "d*,f*";
-      String[] arguments = new String[]{"-d", "/usr/local/bin", "-x", "/usr/bin"};
+      String[] arguments = new String[]{"-d", "/usr/local/bin", "-f", "/usr/bin"};
       Args arg = new Args(schema, arguments);
       assertEquals(arg.getString('d'), "/usr/local/bin");
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 1);
-      assertEquals(arg.errorMessage(), "Argument(s) -x unexpected.");
+      assertEquals(arg.getString('f'), "/usr/bin");
+      assertEquals(arg.cardinality(), 2);
     }
 
     public void testMissingString() throws Exception
     {
-      String schema = "d*";
-      String[] arguments = new String[]{"-d"};
-      Args arg = new Args(schema, arguments);
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 0);
-      assertEquals(arg.errorMessage(), "Could not find string parameter for -d.");
+      try {
+        String schema = "d*";
+        String[] arguments = new String[]{"-d"};
+        Args arg = new Args(schema, arguments);
+        fail();
+      } catch (ArgsException e) {
+        assertEquals(e.getErrorCode(), ArgsException.ErrorCode.MISSING_STRING);
+        assertEquals(e.getErrorArgumentId(), 'd');
+      }
     }
 
     public void testIntegerArgs() throws Exception
     {
       String schema = "n#,p#";
-      String[] arguments = new String[]{"-n", "145", "-x", "123"};
+      String[] arguments = new String[]{"-n", "145", "-p", "123"};
       Args arg = new Args(schema, arguments);
       assertEquals(arg.getInt('n'), 145);
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 1);
-      assertEquals(arg.errorMessage(), "Argument(s) -x unexpected.");
+      assertEquals(arg.getInt('p'), 123);
+      assertEquals(arg.cardinality(), 2);
     }
 
     public void testMissingInteger() throws Exception {
-      String schema = "p#";
-      String[] arguments = new String[]{"-p"};
-      Args arg = new Args(schema, arguments);
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 0);
-      assertEquals(arg.errorMessage(), "Could not find integer parameter for -p.");
+      try {
+        String schema = "p#";
+        String[] arguments = new String[]{"-p"};
+        Args arg = new Args(schema, arguments);
+        fail();
+      } catch (ArgsException e) {
+        assertEquals(e.getErrorCode(), ArgsException.ErrorCode.MISSING_INTEGER);
+        assertEquals(e.getErrorArgumentId(), 'p');
+      }
     }
 
     public void testInvalidInteger() throws Exception {
-      String schema = "p#";
-      String[] arguments = new String[]{"-p", "Test"};
-      Args arg = new Args(schema, arguments);
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 0);
-      assertEquals(arg.errorMessage(), "Argument -p expects an integer but was 'Test'.");
+      try {
+        String schema = "p#";
+        String[] arguments = new String[]{"-p", "Test"};
+        Args arg = new Args(schema, arguments);
+        fail();
+      } catch (ArgsException e) {
+        assertEquals(e.getErrorCode(), ArgsException.ErrorCode.INVALID_INTEGER);
+        assertEquals(e.getErrorArgumentId(), 'p');
+        assertEquals(e.getErrorParameter(), "Test");
+      }
     }
 
     public void testDoubleArgs() throws Exception
     {
       String schema = "f##,r##";
-      String[] arguments = new String[]{"-f", "12.4", "-r", "0.0024", "-t", "0.2"};
+      String[] arguments = new String[]{"-f", "12.4", "-r", "0.0024"};
       Args arg = new Args(schema, arguments);
       assertEquals(arg.getDouble('f'), 12.4);
       assertEquals(arg.getDouble('r'), 0.0024);
-      assertFalse(arg.isValid());
       assertEquals(arg.cardinality(), 2);
-      assertEquals(arg.errorMessage(), "Argument(s) -t unexpected.");
     }
 
     public void testMissingDouble() throws Exception {
-      String schema = "f##";
-      String[] arguments = new String[]{"-f"};
-      Args arg = new Args(schema, arguments);
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 0);
-      assertEquals(arg.errorMessage(), "Could not find double parameter for -f.");
+      try {
+        String schema = "f##";
+        String[] arguments = new String[]{"-f"};
+        Args arg = new Args(schema, arguments);
+      } catch (ArgsException e) {
+        assertEquals(e.getErrorCode(), ArgsException.ErrorCode.MISSING_DOUBLE);
+        assertEquals(e.getErrorArgumentId(), 'f');
+      }
     }
 
     public void testInvalidDouble() throws Exception {
-      String schema = "f##";
-      String[] arguments = new String[]{"-f", "Test"};
-      Args arg = new Args(schema, arguments);
-      assertFalse(arg.isValid());
-      assertEquals(arg.cardinality(), 0);
-      assertEquals(arg.errorMessage(), "Argument -f expects a double but was 'Test'.");
+      try {
+        String schema = "f##";
+        String[] arguments = new String[]{"-f", "Test"};
+        Args arg = new Args(schema, arguments);
+      } catch (ArgsException e) {
+        assertEquals(e.getErrorCode(), ArgsException.ErrorCode.INVALID_DOUBLE);
+        assertEquals(e.getErrorArgumentId(), 'f');
+        assertEquals(e.getErrorParameter(), "Test");
+      }
     }
 
     public void testMixedArgs() throws Exception
@@ -134,7 +156,6 @@ public class ArgsTest
       assertEquals(arg.getBoolean('l'), true);
       assertEquals(arg.getInt('p'), 8080);
       assertEquals(arg.getString('d'), "/usr/lib");
-      assertTrue(arg.isValid());
       assertEquals(arg.cardinality(), 3);
     }
 }
